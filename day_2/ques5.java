71cd5e7edfe8da170a96ad7bca22656d393f3c2b
Vivek <vojha3240@gmail.com>
2026-07-04 20:25:03 +0530

    Add day 2 Java programs

---
+++ day_2/ques5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ques5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+        int r, sum=0;
+         while(n!=0){
+        r=n%10;
+        sum=sum+r;
+        n=n/10;
+    }
+    System.out.print("the sum of the digits is " + sum);
+        sc.close();
+    }
+}
